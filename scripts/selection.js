245522d7e6efa6a4f6faec6cc043c9931a471ced
bazmac81 <bazmac81@gmail.com>
2021-03-07 16:48:48 +0000

    Add: Card Preview section based on user choices

--- scripts/selection.js
+++ scripts/selection.js
@@ -1,35 +1,40 @@
 var choices = [];
-var position = 0; 
+var position = 0;
+var getStarted = document.getElementById('start');
+
+//Capture the section form elements to progress through the card creation
 var imageSelect = document.getElementById('image-select');
 var frameSelect = document.getElementById('frame-select')
-var getStarted = document.getElementById('start');
+var previewCard = document.getElementById('card-msg')
 
 function chooseSelection(e){
     e.preventDefault();
-    var images = imageSelect.getElementsByTagName('input');
-    
-    switch (e.target.getAttribute('id')) {
+    var formID = e.target.getAttribute('id');
+    var images = e.target.getElementsByTagName('input');
+
+    switch (formID) {
         case "image-select":
             position = 0;
             break;
         case "frame-select":
-            positon = 1;
+            position = 1;
             break;
         case "card-msg":
             position = 2;
             break; 
     }
     
-    if(position<2){
-        for (let i =0; i < images.length; i++){
+    console.log(images);
+
+    if(position < 2){
+        for (let i = 0; i < images.length; i++){
             if (images[i].hasAttribute('checked')){
-                console.log(images[i].previousElementSibling.previousElementSibling)
                 choices[position] = images[i].previousElementSibling.previousElementSibling.getAttribute('src');
             }
         }
     }
     else {
-        choices[position] = e.target.getElementsByTagName('input').value;
+        choices[position] = document.getElementById('messageTextarea1').value;
     }
     
     console.log(choices);
@@ -46,13 +51,21 @@ function startCreation(){
     document.getElementById('image-select').classList.toggle('d-none');
 }
 
+function showPreview(){
+    document.getElementById('card-img').setAttribute('src', choices[0]);
+    document.getElementById('framed-msg').style.backgroundImage = "url("+choices[1]+")";
+    document.getElementById('user-msg').innerText = choices[2];
+}
+
 imageSelect.addEventListener('submit', function(e){
     chooseSelection(e);
 });
 frameSelect.addEventListener('submit', function(e){
     chooseSelection(e);
 });
+previewCard.addEventListener('submit', function(e){
+    chooseSelection(e);
+    showPreview();
+});
 
 getStarted.addEventListener('click', startCreation, false);
-
-
