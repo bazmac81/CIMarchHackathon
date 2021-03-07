3f9d1fd92ac86afff8c40d33da27b59c6587bed1
ceciliabinck <cecilia4binck@gmail.com>
2021-03-07 15:44:23 +0000

    Forgot the source code

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -47,7 +47,7 @@
 
 // })
 
-// working prevew of image but doesn't save it
+// working prevew of image but doesn't save it code from D. Pardal and Axel Jorgensen of https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
 
 function preview_image(event) {
             var reader = new FileReader();
